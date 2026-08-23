cf6690a745cfcc42111222118c51103024a56cde
Reza Ahmadi <rezacomplete@gmail.com>
2026-08-23 18:07:18 +1000

    Remove unused methods from repository

--- src/main/java/com/example/AddressBook/repository/ContactRepository.java
+++ src/main/java/com/example/AddressBook/repository/ContactRepository.java
@@ -9,8 +9,6 @@ import java.util.Optional;
 public interface ContactRepository extends JpaRepository<Contact, Long> {
     List<Contact> findByAddressBook_Name(String addressBookName);
     Optional<Contact> findByIdAndAddressBook_Name(Long id, String addressBookName);
-    List<Contact> findByAddressBook_NameAndName(String addressBookName, String name);
-    long deleteByAddressBook_NameAndName(String addressBookName, String name);
     Optional<Contact> findByAddressBook_NameAndNameAndPhone(String addressBookName, String name, String phone);
 
     @Query("SELECT c.name as name, c.phone as phone FROM Contact c GROUP BY c.name, c.phone")
